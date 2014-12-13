1499b2c1af0d1bb5ca024cbd0c2c129b22066be9
Mingo <541871045@qq.com>
2014-12-13 17:08:54 +0800

    返回json

--- src/com/yhq/controllers/UserController.java
+++ src/com/yhq/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.yhq.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yhq.pojo.User;
 import com.yhq.service.IUserService;
@@ -16,9 +18,31 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-    @RequestMapping("insertUser.json")
-    public void insertUser(User user, ModelMap map) {
-	System.out.println(user.getName());
+    /**
+     * 插入数据
+     * 
+     * @param User
+     */
+    /**
+     * 使用：@ResponseBody 标注方法， 该方法返回 Java对象（支持复杂对象），
+     * MappingJacksonHttpMessageConverter 会将对象转换为 JSON 输出
+     * 
+     */
+    @ResponseBody
+    @RequestMapping(value = "insertUser.json")
+    public Map<String, Object> insertUser(User user) {
+	Map<String, Object> map = new HashMap<String, Object>();
 	this.userService.insert(user);
+	map.put("name", user.getName());
+	return map;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "val.json")
+    public Map<String, Object> validataUser(String userName) {
+	Map<String, Object> map = new HashMap<String, Object>();
+	map.put("code", true);
+	map.put("name", userName);
+	return map;
     }
 }
